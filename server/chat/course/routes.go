ce3768c8e9d938b101d066c741d2146066c25be3
Levon Avagyan <levavakian@gmail.com>
2024-11-08 12:21:54 -0500

    starting move

---
+++ server/chat/course/routes.go
@@ -0,0 +1,40 @@
+package course
+
+import (
+	"github.com/gorilla/mux"
+
+	"github.com/levavakian/languagelearn/server/auth"
+)
+
+func SetupRoutes(api *mux.Router) {
+	// Course routes
+	api.HandleFunc("/course", auth.AuthMiddleware(createCourse)).Methods("POST")
+	api.HandleFunc("/courses", auth.AuthMiddleware(getUserCourses)).Methods("GET")
+	api.HandleFunc("/course/{id}", auth.AuthMiddleware(getCourse)).Methods("GET")
+	api.HandleFunc("/course/{id}", auth.AuthMiddleware(updateCourse)).Methods("PUT")
+	api.HandleFunc("/course/{id}", auth.AuthMiddleware(deleteCourse)).Methods("DELETE")
+
+	// Lesson plan routes
+	api.HandleFunc("/course/{id}/lesson-plan", auth.AuthMiddleware(createLessonPlan)).Methods("POST")
+	api.HandleFunc("/course/{id}/lesson-plans", auth.AuthMiddleware(getCourseLessonPlans)).Methods("GET")
+	api.HandleFunc("/course/{id}/lesson-plan/{planId}", auth.AuthMiddleware(getLessonPlan)).Methods("GET")
+	api.HandleFunc("/course/{id}/lesson-plan/{planId}", auth.AuthMiddleware(updateLessonPlan)).Methods("PUT")
+	api.HandleFunc("/course/{id}/lesson-plan/{planId}", auth.AuthMiddleware(deleteLessonPlan)).Methods("DELETE")
+
+	// Lesson routes
+	api.HandleFunc("/course/{id}/lesson", auth.AuthMiddleware(createLesson)).Methods("POST")
+	api.HandleFunc("/course/{id}/lessons", auth.AuthMiddleware(getCourseLessons)).Methods("GET")
+	api.HandleFunc("/course/{id}/lesson/{lessonId}", auth.AuthMiddleware(updateLesson)).Methods("PUT")
+	api.HandleFunc("/course/{id}/lesson/{lessonId}", auth.AuthMiddleware(deleteLesson)).Methods("DELETE")
+
+	// Vocab list routes
+	api.HandleFunc("/course/{id}/vocab", auth.AuthMiddleware(updateVocabList)).Methods("PUT")
+	api.HandleFunc("/course/{id}/vocab", auth.AuthMiddleware(getVocabList)).Methods("GET")
+
+	// Default course routes
+	api.HandleFunc("/course/default", auth.AuthMiddleware(createDefaultCourse)).Methods("POST")
+
+	// Course settings routes
+	api.HandleFunc("/course/{id}/settings", auth.AuthMiddleware(getCourseSettings)).Methods("GET")
+	api.HandleFunc("/course/{id}/settings", auth.AuthMiddleware(updateCourseSettings)).Methods("POST")
+}
