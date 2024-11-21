7e45537e7b0ed5595390fb40fb38aa618eab174b
Levon Avagyan <levavakian@gmail.com>
2024-11-20 20:47:58 -0500

    f

--- prod.sh
+++ prod.sh
@@ -32,7 +32,7 @@ trap 'kill $(jobs -p); exit' INT
 PATH="/usr/lib/postgresql/16/bin:${PATH}" ./db.sh &
 
 # Start the second process
-(cd server && DB_PATH="postgresql://postgres@localhost:5432/postgres?sslmode=disable" ./server) &
+(cd server && DB_PATH="postgresql://postgres@0.0.0.0:5432/postgres?sslmode=disable" ./server) &
 
 # Wait for all processes to complete
 wait -n
