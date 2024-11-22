46820fbf0c254a78ab1f95e6c70f66abc645fab5
Levon Avagyan <levavakian@gmail.com>
2024-11-22 15:26:05 -0500

    css slog with vocab

--- client/src/elements/CourseView/CourseView.tsx
+++ client/src/elements/CourseView/CourseView.tsx
@@ -422,21 +422,24 @@ const VocabList = () => {
     const selectedCourseId = useStateValue(state => state.pageChoice.selectedCourse);
     const setState = useSetStateValue();
     const [viewAll, setViewAll] = useState(false);
-    const [editingKey, setEditingKey] = useState<string>('');
+    
     const [tempAddWord, setTempAddWord] = useState('');
     const [tempAddDefinition, setTempAddDefinition] = useState('');
-    const [tempVocabItems, setTempVocabItems] = useState<Record<string, VocabItem>>({});
 
-    useEffect(() => {
-        setTempVocabItems(settings?.vocabItems || {});
-    }, [settings]);
+    const [editingKey, setEditingKey] = useState<string>('');
+    const [tempEditWord, setTempEditWord] = useState('');
+    const [tempEditDefinition, setTempEditDefinition] = useState('');
 
     if (!settings) {
         return <div className="vocab-list">Loading...</div>;
     }
 
     const handleAddNew = async () => {
-        const newVocabItems = produce(tempVocabItems, draft => {
+        if (!tempAddWord || !tempAddDefinition) {
+            toast.error('Please enter a word and definition', {id: 'vocab-add-error'});
+            return;
+        }
+        const newVocabItems = produce(settings?.vocabItems || {}, draft => {
             draft[tempAddWord] = {
                 word: tempAddWord,
                 definition: tempAddDefinition,
@@ -473,6 +476,57 @@ const VocabList = () => {
         }
     }
 
+    const actionItems = (key: string, value: VocabItem) => {
+        return <div className="vocab-item-actions">
+                <Icon name="bin" scale={12} style={{ cursor: 'pointer', marginTop: '1px', marginLeft: '8px' }} />
+                <Icon name="pencil" scale={12} style={{ cursor: 'pointer', marginTop: '1px', marginLeft: '8px' }} 
+                    onClick={() => {
+                        setEditingKey(key);
+                        setTempEditWord(value.word);
+                        setTempEditDefinition(value.definition);
+                    }}
+                />
+        </div>
+    }
+
+    const editItems = (key: string, value: VocabItem) => {
+        return <div className="vocab-item-actions">
+                <Icon name="x" scale={12} style={{ cursor: 'pointer', marginTop: '1px', marginLeft: '8px' }}
+                    onClick={() => {
+                        if (editingKey === key) {
+                            setEditingKey('null');
+                            setTempEditWord('');
+                            setTempEditDefinition('');
+                        }
+                    }}
+                />
+                <Icon name="check" scale={12} style={{ cursor: 'pointer', marginTop: '1px', marginLeft: '8px' }} />
+        </div>
+    }
+
+    const renderItem = (key: string, value: VocabItem) => {
+        if (editingKey === key) {
+            return <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', flexDirection: 'row', gap: '10px', marginTop: '10px' }}>
+                    <input className="vocab-bubble input" placeholder="Word" value={tempAddWord} onChange={(e) => setTempAddWord(e.target.value)} />
+                    <input className="vocab-bubble input" placeholder="Definition" value={tempAddDefinition} onChange={(e) => setTempAddDefinition(e.target.value)} />
+                    <div className="vocab-bubble add" onClick={handleAddNew} style={{ cursor: 'pointer', marginRight: '0px' }}>Update</div>
+                </div>
+                <div style={{ whiteSpace: 'nowrap',justifyContent: 'flex-end' }}>
+                    {editItems(key, value)}
+                </div>
+            </div>
+        }
+
+        return <div key={key} className="vocab-item-container">
+            <div className="vocab-item">
+                <div className="vocab-word" title={value.word}>{value.word}</div>
+                <div className="vocab-definition" title={value.definition}>{value.definition}</div>
+            </div>
+            {actionItems(key, value)}
+        </div>
+    }
+
     return (
         <div className="vocab-list">
             <div>
@@ -484,16 +538,11 @@ const VocabList = () => {
                 <input className="vocab-bubble input" placeholder="Definition" value={tempAddDefinition} onChange={(e) => setTempAddDefinition(e.target.value)} />
                 <div className="vocab-bubble add" onClick={handleAddNew} style={{ cursor: 'pointer' }}>Add</div>
             </div>
-            {Object.entries(tempVocabItems)
+            {Object.entries(settings.vocabItems || {})
                 .sort(([, a], [, b]) => new Date(b.last_used).getTime() - new Date(a.last_used).getTime())
                 .slice(0, viewAll ? undefined : 4)
-                .map(([key, value]) => (
-                    <div key={key} className="vocab-item">
-                        <div className="vocab-word" title={value.word}>{value.word}</div>
-                        <div className="vocab-definition" title={value.definition}>{value.definition}</div>
-                    </div>
-                ))}
-            {!viewAll && Object.keys(tempVocabItems).length > 4 && (
+                .map(([key, value]) => renderItem(key, value))}
+            {!viewAll && Object.keys(settings.vocabItems || {}).length > 4 && (
                 <div className="vocab-view-all" style={{ cursor: 'pointer' }} onClick={() => setViewAll(true)}>
                     View All
                 </div>
